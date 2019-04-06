d76e299d91822f8884c029a962bbbcadc657a9c7
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-04-06 17:40:01 -0400

    created insert queries to populate to the burgers tables

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ("Hamburger");
+INSERT INTO burgers (name, devoured) VALUES ("Cheeseburger", true);
+INSERT INTO burgers (name, devoured) VALUES ("Bacon cheeseburger", true);
