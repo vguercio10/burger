df3f0e7d9bd1eef1248c85371bfe15e49a33d6f2
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-04-06 20:34:51 -0400

    server.js file completed

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Serving stating contect for the app from the "public" directory in the application directory
 
+app.use(express.static("public"));
 
+// Parse the application body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
+// set handlebars
+var exhbs = require("express-handlebars");
 
+// This is importing the routes and giving the server access to them
+var routes = require("./controllers/burgers_controller");
 
-
-
-
-
-
-
-
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
