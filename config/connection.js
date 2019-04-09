ae6af7ba43d4efb7b5acb7a6c027488e7423c209
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-04-09 09:22:18 -0400

    heroku db connected

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 // Set up mysql connection
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    user: "q5oaewize9g017ct",
+    password: "cilremxkdiqmwav7",
+    database: "ueuechroeq7lg5nb"
+  })
+}
 //   This code is making the connection
   connection.connect(function (err) {
       if (err) {
